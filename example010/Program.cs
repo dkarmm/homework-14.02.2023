f626b2d2a8e71e45f6a9c63c2a306c4a1fdeaa86
dkarmm <dartikarmeev@mail.ru>
2023-02-14 17:43:59 +0500

    create example010.

---
+++ example010/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+// и на выходе показывает вторую цифру этого числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+Console.Clear();
+
+int showSecondDigit(int number)
+{
+    int firstTwoDigit = number / 10;
+    int secondDigit = firstTwoDigit % 10;
+    return secondDigit;
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number < 100 || number > 999)
+{
+    Console.WriteLine("Числое не является трёхзначным.");
+    return;
+}
+int result = showSecondDigit(number);
+Console.WriteLine(result);
+
