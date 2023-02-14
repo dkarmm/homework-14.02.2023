7366acf0703d6cc958341b17676c1221f7db960b
dkarmm <dartikarmeev@mail.ru>
2023-02-14 17:57:26 +0500

    create example013.

---
+++ example013/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа 
+// или сообщает, что третьей цифры нет.
+// 645 -> 5
+// 78 -> третьей цифры нет
+// 32679 -> 6
+Console.Clear();
+
+int showThirdDigit(int number)
+{
+    int thirdDigit = 0;
+    while (number > 999)
+    {
+        number = number / 10;
+    }
+    thirdDigit = number % 10;
+    return thirdDigit;
+}
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number / 100 == 0)
+{
+    Console.WriteLine("Третьей цифры нет.");
+    return;
+}
+int result = showThirdDigit(number);
+Console.WriteLine(result);
